c93eb5ec06c87964dcaba58039eb6f989fb17248
LiasCode <liasraul00@gmail.com>
2025-08-11 04:32:08 -0400

    feat: update plugins source

--- pranx.config.js
+++ pranx.config.js
@@ -1,10 +1,17 @@
+import mdx_plugin from "@mdx-js/esbuild";
 import sassPlugin from "esbuild-plugin-sass";
 import { tailwindPlugin } from "esbuild-plugin-tailwindcss";
 import { defineConfig } from "pranx";
-import { mdx_plugin } from "pranx/plugins";
 
 export default defineConfig({
   esbuild: {
-    plugins: [mdx_plugin(), sassPlugin(), tailwindPlugin()],
+    plugins: [
+      mdx_plugin({
+        jsxImportSource: "preact",
+        jsxRuntime: "automatic",
+      }),
+      sassPlugin(),
+      tailwindPlugin(),
+    ],
   },
 });

e635693301132f491aa678007d8e04cbeba2965b
LiasCode <liasraul00@gmail.com>
2025-08-31 01:24:37 -0400

    chore: improve user name props visuality

--- src/pages/user/[name]/page.tsx
+++ src/pages/user/[name]/page.tsx
@@ -2,14 +2,17 @@ import type { GetServerSidePropsFunction } from "pranx";
 import { Header } from "src/components/Header";
 
 export default function UserDetails(props: { name: string }) {
+  console.log(props);
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
 
       <div class="flex flex-col gap-4 p-6 max-w-3xl mx-auto">
         <h1 class="text-3xl">User Details Page</h1>
-        <span>name: </span>
-        {props.name}
+        <div class="w-fit flex flex-row gap-2">
+          <span class="font-bold capitalize">name: </span>
+          <h1 class="text-orange-400">{props.name}</h1>
+        </div>
       </div>
     </div>
   );

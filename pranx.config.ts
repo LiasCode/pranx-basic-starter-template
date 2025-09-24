1850fcae6fd773dc3d2b83b0725d884e564cb333
LiasCode <liasraul00@gmail.com>
2025-09-24 05:37:41 -0400

    fix: remove deprecated config.csr

--- pranx.config.ts
+++ pranx.config.ts
@@ -1,6 +1,5 @@
 import { PranxConfig } from "pranx";
 
 export default {
-  csr: true,
   esbuild: {},
 } satisfies PranxConfig;
--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import { PranxRouter } from "pranx/client";
-import type { PropsWithChildren } from "preact/compat";
+import { Fragment, type PropsWithChildren } from "preact/compat";
 
 export default function App(props: PropsWithChildren) {
   return (
-    <>
+    <Fragment>
       <PranxRouter mode="spa" />
       {props.children}
-    </>
+    </Fragment>
   );
 }
